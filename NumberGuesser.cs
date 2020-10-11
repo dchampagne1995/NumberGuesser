50505494628e233f428fd563cafc3a7837065f1a
dchampagne1995 <69989583+dchampagne1995@users.noreply.github.com>
2020-10-10 18:24:11 -0700

    added play again feature with case insensitivity for the response

--- NumberGuesser.cs
+++ NumberGuesser.cs
@@ -7,6 +7,59 @@ namespace NumberGuesser
         static void Main(string[] args)
         {
 
+            printAppDetail();
+
+            //Ask user's name and get input
+            Console.WriteLine("What is your name?");
+            String userName = Console.ReadLine();
+
+            while (true)
+            {
+                //Setting the random number
+                Random random = new Random();
+                int numberAnswer = random.Next(1, 10);
+
+                //Initializing user guess variable
+                int userGuess = 0;
+
+                Console.WriteLine("Hello {0}, Choose a number between 1 and 10", userName);
+
+                //Getting user input and converting to int
+                String stringUserGuess = Console.ReadLine();
+                userGuess = Int32.Parse(stringUserGuess);
+
+                //Comparing user guess to correct number
+                while (userGuess != numberAnswer)
+                {
+                    //Re asking for user input and converting again
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number guess again!");
+                    stringUserGuess = Console.ReadLine();
+                    userGuess = Int32.Parse(stringUserGuess);
+                    Console.ResetColor();
+
+                }
+
+                Console.WriteLine("Congratulations you guessed the number! {0} ", userGuess);
+
+                //Play again feature
+                Console.WriteLine("Do you want to play again Y/N");
+                string userResponse = Console.ReadLine();
+                if (string.Equals(userResponse, "Y", StringComparison.OrdinalIgnoreCase))
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write("Ok Goodbye");
+                    return;
+                }
+
+            }
+        }
+
+      static void printAppDetail()
+        {
             //Initializing application vars
             String appName = "Number Guessing Application";
             String appVersion = "1.0.0";
@@ -16,42 +69,12 @@ namespace NumberGuesser
             //Changing text colour when displaying app details
             Console.ForegroundColor = ConsoleColor.Cyan;
 
-            Console.WriteLine("{0}: Version {1} Created By {2}",appName, appVersion, creator);
+            Console.WriteLine("{0}: Version {1} Created By {2}", appName, appVersion, creator);
 
             //Resetting text colour
             Console.ResetColor();
 
-            //Ask user's name and get input
-            Console.WriteLine("What is your name?");
-            String userName = Console.ReadLine();
-
-            //Setting the random number
-            Random random = new Random();
-            int numberAnswer = random.Next(1,10);
-
-            //Initializing user guess variable
-            int userGuess = 0;
-
-            Console.WriteLine("Hello {0}, Choose a number between 1 and 10", userName);
-
-            //Getting user input and converting to int
-            String stringUserGuess = Console.ReadLine();
-            userGuess = Int32.Parse(stringUserGuess);
-
-            //Comparing user guess to correct number
-            while(userGuess != numberAnswer)
-            {
-                //Re asking for user input and converting again
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Wrong number guess again!");
-                stringUserGuess = Console.ReadLine();
-                userGuess = Int32.Parse(stringUserGuess);
-                Console.ResetColor();
-
-            }
-
-            Console.WriteLine("Congratulations you guessed the number! {0} ", userGuess);
-
         }
+
     }
 }
